078c87460220d4c6b7ae439b43e2ae9320d9948b
DariaRusovich <rusovichdasha41@gmail.com>
2021-08-25 17:54:14 +0300

    add-use-satate

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import "./Dropdown.css";
 
+
 export default function Dropdown({ data, children }) {
+    const [open, setOpen] = useState(false)
   return (
     <div className="dropdown">
         <div>
-      <button className="dropdown-btn">{children}</button>
-      {data && data.length > 0 && (
+      <button onClick={() => setOpen(prev => !prev)} className="dropdown-btn">{children}</button>
+     {open && data && data.length > 0 && (
         <ul className="dropdown-list">
           {data.map((item) => (
             <li key={item}>{item}</li>
           ))}
         </ul>
-      )}
+      )} 
       </div>
     </div>
   );
