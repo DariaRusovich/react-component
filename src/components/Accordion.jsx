ef267f31feb0e59cb14abb10727e0752edd4c9ec
DariaRusovich <rusovichdasha41@gmail.com>
2021-08-30 17:51:37 +0300

    add-arrow

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -5,8 +5,8 @@ export default function Accordion({ children, title, active, setActive }) {
     <div className="accordion-item">
       <div onClick={() => setActive(title)} className="accordion-heading">
         <h2 className="accordion-header">{title}</h2>
-        <span>
-            {(active === title ? 'X' : '|||')}
+        <span className='accordion-icon'>
+            {(active === title ? '▲' : '▼')}
         </span>
       </div>
       <div className={(active === title ? 'show' : '') + ' accordion-body'}>
